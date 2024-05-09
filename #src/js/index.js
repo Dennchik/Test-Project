d1a35fe9a033146215abd353019ea82ed286c719
Alexandr <104072970+Dennchik@users.noreply.github.com>
2024-05-10 01:35:10 +0300

    10 Май 2024

--- #src/js/index.js
+++ #src/js/index.js
@@ -1,10 +1,9 @@
 import React, { StrictMode } from "react";
 import * as ReactDOMClient from "react-dom/client";
-import { createRoot } from 'react-dom/client';
 import App from './app/App.jsx';
 // -----------------------------------------------------------------------------
 
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, Navigate, Link, BrowserRouter } from 'react-router-dom';
 // -----------------------------------------------------------------------------
 
 import { Homepage } from "./pages/Homepage.jsx";
@@ -15,20 +14,20 @@ import { AuthProvider } from './hoc/AuthProvider.jsx';
 // -----------------------------------------------------------------------------
 
 
-// const rootElement = document.getElementById("page");
-// ReactDOMClient.createRoot(rootElement);
-createRoot(document.getElementById('page')).render(
+const rootElement = document.getElementById("page");
+const root = ReactDOMClient.createRoot(rootElement);
+root.render(
 	<StrictMode>
 		<BrowserRouter basename="/Test-Project/build">
 			<AuthProvider>
 				<Routes>
 					<Route path="/" element={<Layout />}>
-						<App />
 						<Route path="/" element={<Homepage />} />
 						<Route path="/about" element={<Aboutpage />} />
 					</Route>
 				</Routes>
 			</AuthProvider>
 		</BrowserRouter>
-	</StrictMode>
+	</StrictMode>,
+	document.getElementById('page')
 );

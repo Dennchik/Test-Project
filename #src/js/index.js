8a3c5c45518003bd547be2df9cf30226599543f4
Alexandr <104072970+Dennchik@users.noreply.github.com>
2024-05-10 01:31:36 +0300

    10 Май 2024

--- #src/js/index.js
+++ #src/js/index.js
@@ -4,17 +4,31 @@ import { createRoot } from 'react-dom/client';
 import App from './app/App.jsx';
 // -----------------------------------------------------------------------------
 
-import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 // -----------------------------------------------------------------------------
 
+import { Homepage } from "./pages/Homepage.jsx";
+import { Aboutpage } from "./pages/Aboutpage.jsx";
+// -----------------------------------------------------------------------------
+import { Layout } from './components/Layout.jsx';
+import { AuthProvider } from './hoc/AuthProvider.jsx';
 // -----------------------------------------------------------------------------
 
-// -----------------------------------------------------------------------------
+
+// const rootElement = document.getElementById("page");
+// ReactDOMClient.createRoot(rootElement);
 createRoot(document.getElementById('page')).render(
 	<StrictMode>
-		{/* <BrowserRouter> */}
 		<BrowserRouter basename="/Test-Project/build">
-			<App />
+			<AuthProvider>
+				<Routes>
+					<Route path="/" element={<Layout />}>
+						<App />
+						<Route path="/" element={<Homepage />} />
+						<Route path="/about" element={<Aboutpage />} />
+					</Route>
+				</Routes>
+			</AuthProvider>
 		</BrowserRouter>
 	</StrictMode>
 );

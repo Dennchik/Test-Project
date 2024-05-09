6ec36336d8ff2995f390bcf0f5fa7734d89ce2f2
Alexandr <104072970+Dennchik@users.noreply.github.com>
2024-05-10 01:07:04 +0300

    10  Май 2024
    {/* <BrowserRouter> */}
    <BrowserRouter basename="/Test-Project/build">

--- #src/js/index.js
+++ #src/js/index.js
@@ -12,7 +12,8 @@ import { BrowserRouter } from 'react-router-dom';
 // -----------------------------------------------------------------------------
 createRoot(document.getElementById('page')).render(
 	<StrictMode>
-		<BrowserRouter>
+		{/* <BrowserRouter> */}
+		<BrowserRouter basename="/Test-Project/build">
 			<App />
 		</BrowserRouter>
 	</StrictMode>
